da82f85ba97ff02530628a360032c6fde56d2ccf
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-14 00:37:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,4 +18,13 @@ get '/say/:number/:phrase' do
   "#{@phrase}.@number.times"
 end
 
+get get '/say/:word1/:word2/:word3/:word4/:word5' do
+  @word1 = params[:word1]
+  @word2 = params[:word2]
+  @word3 = params[:word3]
+  @word4 = params[:word4]
+  @word5 = params[:word5]
+end
+end
+
 end
